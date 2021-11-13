f6880ea259eb29af173f1de8a690835c9fbbe337
Inteli_Soft-One <Inteli_Soft-One@LAPTOP-K2OJ3KB6>
2021-11-12 22:16:54 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace HolaMundo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btValida = new System.Windows.Forms.Button();
+            this.txtPrincipal = new System.Windows.Forms.TextBox();
+            this.txtSecundario = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btValida
+            // 
+            this.btValida.Location = new System.Drawing.Point(571, 188);
+            this.btValida.Name = "btValida";
+            this.btValida.Size = new System.Drawing.Size(140, 79);
+            this.btValida.TabIndex = 0;
+            this.btValida.Text = "Valida";
+            this.btValida.UseVisualStyleBackColor = true;
+            this.btValida.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtPrincipal
+            // 
+            this.txtPrincipal.Location = new System.Drawing.Point(264, 116);
+            this.txtPrincipal.Name = "txtPrincipal";
+            this.txtPrincipal.Size = new System.Drawing.Size(181, 22);
+            this.txtPrincipal.TabIndex = 1;
+            // 
+            // txtSecundario
+            // 
+            this.txtSecundario.Location = new System.Drawing.Point(264, 160);
+            this.txtSecundario.Name = "txtSecundario";
+            this.txtSecundario.Size = new System.Drawing.Size(181, 22);
+            this.txtSecundario.TabIndex = 2;
+            this.txtSecundario.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtSecundario);
+            this.Controls.Add(this.txtPrincipal);
+            this.Controls.Add(this.btValida);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btValida;
+        private System.Windows.Forms.TextBox txtPrincipal;
+        private System.Windows.Forms.TextBox txtSecundario;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HolaMundo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string primerTxt = txtPrincipal.Text;
+            string secondTxt = txtSecundario.Text;
+
+            // Regex regex = new Regex(@"^(.{0,7}|[^0-9]*|[^A-Z])$");
+            //Match match = regex.Match(primerTxt);
+
+            //@"^(.{0,7}|[^0-9]*|[^A-Z])$"
+            //Regex regex = new Regex("@^((?=.*/d)(?=.*[A - Z])(?=.*/W).{ 8,8}))$");
+            Regex regex = new Regex(@"^[0-9a-zA-Z´=%\@#$&()=¿?\\\/<>´¨¡!*.;_+:-]+$");
+            if (regex.IsMatch(primerTxt) && primerTxt == secondTxt) { MessageBox.Show("Correcto Contraseña Valida"); }
+            else { MessageBox.Show("hola tu contraseña es no valida"); }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
